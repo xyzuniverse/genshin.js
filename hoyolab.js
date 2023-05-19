38c1ffd1dc2e3f45c0dda07aee26e1b21a72b5e7
xyzuniverse <dev.xyzuniverse@gmail.com>
2023-05-19 19:45:34 +0800

    genshin.js: Add Genshin battle chronicle
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- hoyolab.js
+++ hoyolab.js
@@ -8,6 +8,8 @@ const Errors = require('./Errors.json');
  */
 class Client {
     constructor(account_id, cookie_token, game, server) {
+        this.account_id = account_id;
+        this.cookie_token = cookie_token;
         this.cookieToken = ['account_id=' + account_id, 'cookie_token=' + cookie_token].join('; ');
         this.game = game;
         this.server = server;
@@ -24,7 +26,8 @@ class Client {
                 game_biz: Handler.recognizeGame(this.game),
             }
         });
-        return result;
+        if (result.data.list) return result.data.list;
+        else return result
     };
 
     async claimRedeemCode(code) {
@@ -81,6 +84,50 @@ class Client {
             }
         })
         return result;
+    }
+
+    async genshinBattleChronicle() {
+        var userinfo = await (await this.getUserInformation()).find(v => v.region.includes(this.server.toLowerCase()));
+        try {
+            var attempt = 0;
+            var retry = function(data) {
+                if (attempt > 3) {
+                    throw new Error("Error when trying to get data, please recheck your cookie token or uid!")
+                }
+                if (data.retcode === -10001) {
+                    return attempt += 1
+                }
+            }
+            do {
+                const { data } = await axios("https://bbs-api-os.hoyolab.com/game_record/genshin/api/index", {
+                    method: "GET",
+                    headers: {
+                        'cookie': this.cookieToken,
+                        'user-agent': Routes['user-agent'],
+                        'ds': this.generateDSToken(),
+                        'x-rpc-app_version': '1.5.0',
+                        'x-rpc-client_type': '5',
+                        'x-rpc-language': 'en-us'
+                    },
+                    params: {
+                        server: userinfo.region,
+                        role_id: userinfo.game_uid
+                    }
+                })
+                return data;
+            } while(retry(data))
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    generateDSToken() {
+        const DS_SALT = '6s25p5ox5y14umn1p61aqyyvbvvl3lrt'
+        var date = new Date()
+        var time = Math.floor(date.getTime()/1000)
+        const r = (Math.random() + 1).toString(36).substring(6);
+        var hash = require('crypto').createHash('md5').update(`salt=${DS_SALT}&t=${time}&r=${r}`).digest('hex');
+        return `${time},${r},${hash}`
     }
 }
 
